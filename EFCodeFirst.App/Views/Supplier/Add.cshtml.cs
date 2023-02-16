7857dc37faea9227e65a9470aef2860a3d3a3eb0
Kacie Quiogue <kacie.quiogue@benilde.edu.ph>
2023-02-16 15:45:03 +0800

    added views for Supplier

---
+++ EFCodeFirst.App/Views/Supplier/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EFCodeFirst.App.Views.Supplier
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
