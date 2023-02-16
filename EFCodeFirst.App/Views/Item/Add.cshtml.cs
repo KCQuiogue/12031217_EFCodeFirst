fbb7a10cd162aaeab4d23f4980e4ebeb015994fc
Kacie Quiogue <kacie.quiogue@benilde.edu.ph>
2023-02-16 15:59:29 +0800

    added Item Views

---
+++ EFCodeFirst.App/Views/Item/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EFCodeFirst.App.Views.Item
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EFCodeFirst.App/Views/Item/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EFCodeFirst.App.Views.Item
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
