6e6cd704a1dffa8439e6e752a49f68afad8f5f1c
Kacie Quiogue <kacie.quiogue@benilde.edu.ph>
2023-02-16 17:20:29 +0800

    added controller and views for ItemOrder

---
+++ EFCodeFirst.App/Views/ItemOrder/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EFCodeFirst.App.Views.ItemOrder
+{
+    public class ViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
